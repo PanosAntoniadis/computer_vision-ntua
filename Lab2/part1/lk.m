8edd2aaac1af10ba51dc7d2752c9e7f36785471d
PanosAntoniadis <pantoniadis97@gmail.com>
2019-05-28 19:11:21 +0300

    Add 1.4 part

--- Lab2/part1/lk.m
+++ Lab2/part1/lk.m
@@ -1,73 +1,71 @@
+function [d_x,d_y] = lk(I1, I2, rho, epsilon, d_x0, d_y0)
+% lk - Lucas-Kanade algorithm
+% 
+% Usage:
+%         [d_x,d_y] = lk(I1, I2, rho, epsilon, d_x0, d_y0)
+% 
+% Description:
+% Computes the optical flow between two images.
+% 
+% In:
+%  I1, I2: input images
+%  rho: standard deviation of Gaussian
+%  epsilon: parameter 
+%  d_x0, d_y0: initialization of vector field d
+%
+% Out:
+%   d_x, d_y: vector field d
+%
 
-function [d_x,d_y] = lk(I1,I2,rho,epsilon,d_x0,d_y0)
+% Convert iamges to range [0, 1]
+I1 = mat2gray(I1);
+I2 = mat2gray(I2);
 
-load("skinSamplesRGB.mat");
-[mean_CbCr, cov_CbCr] = trainPdf(skinSamplesRGB);
+%Define the times d is re-computed
+k=50;
 
-boundingbox1 = findFace(I1,mean_CbCr, cov_CbCr);  % (x,y,width,height)
-bx1 = boundingbox1(1);
-by1 = boundingbox1(2);
-bw1 = boundingbox1(3);
-bh1 = boundingbox1(4);
-boundingbox2 = findFace(I2,mean_CbCr, cov_CbCr);  % (x,y,width,height)
-bx2 = boundingbox2(1);
-by2 = boundingbox2(2);
-bw2 = boundingbox2(3);
-bh2 = boundingbox2(4);
+% Create the gaussian with standard deviation rho
+n = (ceil(3*rho)*2) + 1;
+G_r = fspecial('gaussian', [n n], rho);
 
-bw=max(bw1,bw2);
-bh=max(bh1,bh2);
+% Define a meshgrid
+[x_0,y_0] = meshgrid(1:size(I1,2), 1:size(I1,1));
+[A1_0, A2_0] = gradient(I1);
 
-I1 = I1(ceil(by1:(by1+bh)),ceil(bx1:(bx1+bw))); % crop the face of the first frame
+% Initialization of vector d
+d_x = d_x0;
+d_y = d_y0;
 
-I2 = I2(ceil(by2:(by2+bh)),ceil(bx2:(bx2+bw))); % crop the face of the next frame
-% (SOS) : issue with different dimensions of bounding boxes
-%imshow(I1);
-%figure;
-%hold on;
-%imshow(I2);
-
-I1=double(I1);
-I2=double(I2(:,:));
-k=10; %Define the times d is re-computed
-%--------------------------------------------------------------------------
-%Create the gaussian with standard deviation rho
-n=(ceil(3*rho)*2)+1;
-G_r=fspecial('gaussian',[n n],rho);
-
-[x_0,y_0]=meshgrid(1:size(I1,2),1:size(I1,1));
-[A1_0,A2_0]=gradient(I1);
-dx=d_x0;
-dy=d_y0;
 for i=1:k
     
-I_prev=interp2(I1(:,:),x_0+dx,y_0+dy, 'linear', 0);
-A1=interp2(A1_0(:,:), x_0+dx, y_0+dy,'linear',0);
-A2=interp2(A2_0(:,:),x_0+dx, y_0+dy,'linear',0);
-E=I2-I_prev;
-% U = (B.^-1)*C
-b11=imfilter(A1.^2,G_r,'symmetric')+epsilon;
-b12=imfilter(A1.*A2,G_r,'symmetric');
-b22=imfilter(A2.^2,G_r,'symmetric')+epsilon;
-B=[b11 b12; b12 b22];
-c1=imfilter(A1.*E,G_r,'symmetric');
-c2= imfilter(A2.*E,G_r,'symmetric');
-C=[c1;c2];
-ux=b11.*c1+b12.*c2;
-uy=b12.*c1+b22.*c2;
-%ux=U(1);
-%uy=U(2);
-dx=dx+ux;
-dy=dy+uy;
+    I_prev = interp2(I1, x_0 + d_x, y_0 + d_y, 'linear', 0);
+    % Compute A
+    A1 = interp2(A1_0, x_0 + d_x, y_0 + d_y, 'linear', 0);
+    A2 = interp2(A2_0, x_0 + d_x, y_0 + d_y, 'linear', 0);
+    E = I2 - I_prev;
+    
+    % Compute b
+    b11 = imfilter(A1.^2, G_r, 'symmetric') + epsilon;
+    b12 = imfilter(A1.*A2, G_r, 'symmetric');
+    b22 = imfilter(A2.^2, G_r, 'symmetric') + epsilon;
+    
+    % Compute c
+    c1 = imfilter(A1.*E, G_r, 'symmetric');
+    c2 = imfilter(A2.*E, G_r, 'symmetric');
 
-%if (norm(ux)<thr && norm(uy)<thr)
-%   break;
-%end
+    % Compute u
+    det = b11 .* b22 - b12.^2;
+    u_x = (b22.*c1 - b12.*c2)./det;
+    u_y = (-b12.*c1 + b22.*c2)./det;
 
+    % Update d
+    d_x = d_x + u_x;
+    d_y = d_y + u_y;
+
+    %if (norm(ux)<thr && norm(uy)<thr)
+    %   break;
+    %end
 end
-
-d_x=dx;
-d_y=dy;
 
 
 end
--- Lab2/part1/part1.m
+++ Lab2/part1/part1.m
@@ -17,10 +17,10 @@ load("skinSamplesRGB.mat");
 % Compute bounding box.
 boundingBox = findFace(I, mean_CbCr, cov_CbCr);
 
-figure;
-imshow(I);
-hold on;
-rectangle('Position',boundingBox, 'EdgeColor', 'g');
+% figure;
+% imshow(I);
+% hold on;
+% rectangle('Position', boundingBox, 'EdgeColor', 'g');
 
 
 
@@ -28,12 +28,81 @@ rectangle('Position',boundingBox, 'EdgeColor', 'g');
 %   1.2: Lucas-Kanade algorithm    %
 % -------------------------------- %
 
+% Define parameters
+rho = 3;
+epsilon = 0.05;
+d_x0 = 0;
+d_y0 = 0;
+
+% Read two input images
+I1 = imread('./GSLframes/69.png');
+I2 = imread('./GSLframes/70.png');
+
+% Compute their bounding boxes
+boundingBox1 = findFace(I1, mean_CbCr, cov_CbCr);
+x1 = boundingBox1(1);
+y1 = boundingBox1(2);
+w1 = boundingBox1(3);
+h1 = boundingBox1(4);
+
+%boundingBox2 = findFace(I2, mean_CbCr, cov_CbCr);
+%x2 = boundingBox2(1);
+%y2 = boundingBox2(2);
+
+% Keep height and width of the first image
+width = w1;
+height = h1;
+
+% Compute images in the bounding box and
+% call Lucas Kanade function
+I1_face = I1(ceil(y1: y1+height), ceil(x1: x1+width));
+I2_face = I2(ceil(y1: y1+height), ceil(x1: x1+width));
+[d_x, d_y] = lk(I1_face, I2_face, rho, epsilon, d_x0, d_y0);
+
+% Plot d as a vector field
+d_x_r = imresize(d_x, 0.3);
+d_y_r = imresize(d_y, 0.3);
+%quiver(-d_x_r, -d_y_r);
 
 
+% ----------------------------------- %
+%   1.4: Compute global face shift    %
+% ----------------------------------- %
 
+% Define threshold
+threshold = 1;
 
+% Compute global face shift
+[displ_x, displ_y] = displ(d_x, d_y, threshold);
 
+I = imread('./GSLframes/1.png');
+boundingBox = findFace(I, mean_CbCr, cov_CbCr);
+x = boundingBox(1);
+y = boundingBox(2);
+w = boundingBox(3);
+h = boundingBox(4);
+figure;
+imshow(I);
+hold on;
+rectangle('Position',boundingBox, 'EdgeColor', 'g');
+hold off
 
+for k = 1:70
+    I_prev = imread(strcat('./GSLframes/', strcat(num2str(k), '.png')));
+    I_curr = imread(strcat('./GSLframes/', strcat(num2str(k+1), '.png')));
+    I_prev_face = I_prev(ceil(y: y+height), ceil(x: x+width));
+    I_curr_face = I_curr(ceil(y: y+height), ceil(x: x+width));
+    [d_x, d_y] = lk(I_prev_face, I_curr_face, rho, epsilon, d_x0, d_y0);
+    [displ_x, displ_y] = displ(-d_x, -d_y, threshold);
+    x = x + displ_x;
+    y = y + displ_y;
+
+    pause(0.000005);
+    imshow(I_curr);
+    hold on;
+    rectangle('Position', [x, y, w, h], 'EdgeColor', 'g');
+    hold off
+end
 
 
 
